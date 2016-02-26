b1e8770cea3bcdd3514b408db2e5f66550234517
avix101 <stashablank@gmail.com>
2016-02-25 22:52:19 -0500

    Stage One Level Gen (Semi-Working)

--- Once A Rogue/Once A Rogue/Game1.cs
+++ Once A Rogue/Once A Rogue/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Reflection; //Needed for string to variable calls
 
 namespace Once_A_Rogue
 {
@@ -11,6 +12,39 @@ namespace Once_A_Rogue
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        //Declare a grid to keep track of level space
+        string [,] gridSystem;
+        const int ROWS = 9;
+        const int COLUMNS = 9;
+
+        int numRooms;
+
+        //Camera Mods
+        int xMod = 0;
+        int yMod = 0;
+
+        //Declare Room Textures
+        Texture2D allDirections;
+        Texture2D left;
+        Texture2D up;
+        Texture2D right;
+        Texture2D down;
+        Texture2D leftDown;
+        Texture2D leftRight;
+        Texture2D leftUp;
+        Texture2D upRight;
+        Texture2D upDown;
+        Texture2D rightDown;
+        Texture2D leftUpRight;
+        Texture2D leftUpDown;
+        Texture2D leftRightDown;
+        Texture2D upRightDown;
+
+
+
+
+        bool buildLevel;
 
         public Game1()
         {
@@ -29,9 +63,20 @@ namespace Once_A_Rogue
         {
             // TODO: Add your initialization logic here
 
+            //Allow the game to run in fullscreen
             graphics.IsFullScreen = true;
-
             graphics.ApplyChanges();
+
+            //Run Level Builder! Generate the first level
+            gridSystem = new string[ROWS, COLUMNS];
+
+            LevelBuilder builderAlpha = new LevelBuilder();
+
+            numRooms = 30;
+
+            gridSystem = builderAlpha.BuildLevel(gridSystem, 10);
+
+            buildLevel = true;           
 
             base.Initialize();
         }
@@ -46,6 +91,25 @@ namespace Once_A_Rogue
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            //Initialize all room textures
+
+            allDirections = Content.Load<Texture2D>("AllDirections-RoomCode.png");
+            left = Content.Load<Texture2D>("Left-RoomCode.png");
+            up = Content.Load<Texture2D>("Up-RoomCode.png");
+            right = Content.Load<Texture2D>("Right-RoomCode.png");
+            down = Content.Load<Texture2D>("Down-RoomCode.png");
+            leftDown = Content.Load<Texture2D>("Up-RoomCode.png");
+            leftRight = Content.Load<Texture2D>("LeftRight-RoomCode.png");
+            leftUp = Content.Load<Texture2D>("LeftUp-RoomCode.png");
+            upRight = Content.Load<Texture2D>("UpRight-RoomCode.png");
+            upDown = Content.Load<Texture2D>("UpDown-RoomCode.png");
+            rightDown = Content.Load<Texture2D>("RightDown-RoomCode.png");
+            leftUpRight = Content.Load<Texture2D>("LeftUpRight-RoomCode.png");
+            leftUpDown = Content.Load<Texture2D>("LeftUpDown-RoomCode.png");
+            leftRightDown = Content.Load<Texture2D>("LeftRightDown-RoomCode.png");
+            upRightDown = Content.Load<Texture2D>("UpRightDown-RoomCode.png");
+
         }
 
         /// <summary>
@@ -67,7 +131,28 @@ namespace Once_A_Rogue
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // TODO: Add your update logic here 
+
+            //Camera //Modular Testing Tool * Remove in Final Cut
+        
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Right))
+            {
+                xMod += 10;
+            }
+            if (state.IsKeyDown(Keys.Left))
+            {
+                xMod -= 10;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                yMod -= 10;
+            }
+            if (state.IsKeyDown(Keys.Down))
+            {
+                yMod += 10;
+            }
 
             base.Update(gameTime);
         }
@@ -81,6 +166,124 @@ namespace Once_A_Rogue
             GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            //If a new level has been generated update and redraw frame
+            if (buildLevel)
+            {
+                //Reset build call
+                //buildLevel = false;
+
+                int rowIndex = 0;
+                int columnIndex = 0;
+
+                while (rowIndex < ROWS)
+                {
+                    while (columnIndex < COLUMNS)
+                    {
+                        if (gridSystem[rowIndex, columnIndex] != null)
+                        {
+                            int xCoord = ((GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2) + xMod);
+                            int yCoord = ((GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2) + yMod);
+
+                            xCoord += ((columnIndex - 4) * 640);
+                            yCoord += ((rowIndex - 4) * 400);
+
+                            Texture2D room;
+
+                            switch(gridSystem[rowIndex,columnIndex])
+                            {
+                                case ("allDirections"):
+
+                                    room = allDirections;
+                                    break;
+
+                                case ("left"):
+
+                                    room = left;
+                                    break;
+
+                                case ("up"):
+
+                                    room = up;
+                                    break;
+
+                                case ("right"):
+
+                                    room = right;
+                                    break;
+
+                                case ("down"):
+
+                                    room = down;
+                                    break;
+
+                                case ("leftDown"):
+
+                                    room = leftDown;
+                                    break;
+
+                                case ("leftRight"):
+
+                                    room = leftRight;
+                                    break;
+
+                                case ("leftUp"):
+
+                                    room = leftUp;
+                                    break;
+
+                                case ("upRight"):
+
+                                    room = upRight;
+                                    break;
+
+                                case ("upDown"):
+
+                                    room = upDown;
+                                    break;
+
+                                case ("rightDown"):
+
+                                    room = rightDown;
+                                    break;
+
+                                case ("leftUpRight"):
+
+                                    room = leftUpRight;
+                                    break;
+
+                                case ("leftUpDown"):
+
+                                    room = leftUpDown;
+                                    break;
+
+                                case ("leftRightDown"):
+
+                                    room = leftRightDown;
+                                    break;
+
+                                case ("upRightDown"):
+
+                                    room = upRightDown;
+                                    break;
+
+                                default:
+                                    room = allDirections;
+                                    break;
+                            }
+
+                            spriteBatch.Draw(room, new Vector2(xCoord, yCoord), Color.White);
+                        }
+                        columnIndex++;
+                    }
+                    columnIndex = 0;
+                    rowIndex++;
+                }
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- Once A Rogue/Once A Rogue/LevelBuilder.cs
+++ Once A Rogue/Once A Rogue/LevelBuilder.cs
@@ -13,6 +13,172 @@ namespace Once_A_Rogue
 {
     class LevelBuilder
     {
-        
+        public LevelBuilder()
+        {
+            //Init
+        }
+
+        public string[,] BuildLevel(string[,] grid, int numRooms)
+        {
+            Dictionary<string, Boolean> roomUsed = new Dictionary<string, bool>();
+
+            //Populate the center of the grid with a four way room
+            grid[4, 4] = "allDirections";
+
+            roomUsed["44"] = true;
+
+            int roomCount = 1;
+
+            Random random = new Random(); 
+
+            string[] possibleRoomsMultiLeft = new string[] {"allDirections", "leftDown", "leftRight", "leftUp",
+            "leftUpRight", "leftUpDown", "leftRightDown"};
+
+            string[] possibleRoomsMultiRight = new string[] {"allDirections", "leftRight", "upRight",
+            "rightDown", "leftUpRight", "leftRightDown", "upRightDown"};
+
+            string[] possibleRoomsMultiDown = new string[] {"allDirections", "leftDown",
+            "upDown", "rightDown", "leftUpDown", "leftRightDown", "upRightDown"};
+
+            string[] possibleRoomsMultiUp = new string[] {"allDirections", "leftUp", "upRight",
+            "upDown", "leftUpRight", "leftUpDown", "upRightDown"};
+
+            string[] possibleRoomsSingle = new string[] {"left", "up", "right", "down"};
+
+            Queue<string> nodeQueue = new Queue<string>();
+
+            //Prepopulate Queue with correct nodes
+
+            //1st digit: 1 = left connector, 2 = up connector, 3 = right connector, 4 = down connector
+            //2nd digit: x coord
+            //3rd digit: y coord
+
+            nodeQueue.Enqueue("445");
+            nodeQueue.Enqueue("243");
+            nodeQueue.Enqueue("154");
+            nodeQueue.Enqueue("334");
+
+            while(nodeQueue.Count != 0)
+            {
+                string node = nodeQueue.Dequeue();
+
+                string selectedRoom = "";
+
+                if(roomCount <= numRooms && !roomUsed.ContainsKey(node.Substring(1)))
+                {
+                    switch (node[0])
+                    {
+                        case ('3'):
+                            selectedRoom = possibleRoomsMultiLeft[random.Next(0, possibleRoomsMultiLeft.Length)];
+                            break;
+
+                        case ('4'):
+                            selectedRoom = possibleRoomsMultiUp[random.Next(0, possibleRoomsMultiUp.Length)];
+                            break;
+
+                        case ('1'):
+                            selectedRoom = possibleRoomsMultiRight[random.Next(0, possibleRoomsMultiRight.Length)];
+                            break;
+
+                        case ('2'):
+                            selectedRoom = possibleRoomsMultiDown[random.Next(0, possibleRoomsMultiDown.Length)];
+                            break;
+                    }
+
+                    int xCoord = int.Parse(node[1].ToString());
+                    int yCoord = int.Parse(node[2].ToString());
+
+                    //BAD CODE
+                    if(xCoord == 9 || yCoord == 9)
+                    {
+                        xCoord = 8;
+                        yCoord = 8;
+                    }
+
+                    grid[xCoord, yCoord] = selectedRoom;
+
+                    string roomNum = xCoord.ToString();
+                    roomNum += yCoord.ToString();
+
+                    roomUsed[roomNum] = true;
+
+                    selectedRoom = selectedRoom.ToUpper();
+
+                    roomCount += 1;
+
+                    if (selectedRoom.Contains("LEFT"))
+                    {
+                        roomNum = "3";
+                        roomNum += (xCoord - 1).ToString();
+                        roomNum += yCoord.ToString();
+                        nodeQueue.Enqueue(roomNum);
+                    }
+                    if (selectedRoom.Contains("UP"))
+                    {
+                        roomNum = "4";
+                        roomNum += xCoord.ToString();
+                        roomNum += (yCoord - 1).ToString();
+                        nodeQueue.Enqueue(roomNum);
+                    }
+                    if (selectedRoom.Contains("RIGHT"))
+                    {
+                        roomNum = "1";
+                        roomNum += (xCoord + 1).ToString();
+                        roomNum += yCoord.ToString();
+                        nodeQueue.Enqueue(roomNum);
+                    }
+                    if (selectedRoom.Contains("DOWN"))
+                    {
+                        roomNum = "2";
+                        roomNum += xCoord.ToString();
+                        roomNum += (yCoord + 1).ToString();
+                        nodeQueue.Enqueue(roomNum);
+                    }
+                }
+
+                else if(roomCount > numRooms && !roomUsed.ContainsKey(node.Substring(1)))
+                {
+                    switch (node[0])
+                    {
+                        case ('3'):
+                            selectedRoom = "left";
+                            break;
+
+                        case ('4'):
+                            selectedRoom = "up";
+                            break;
+
+                        case ('1'):
+                            selectedRoom = "right";
+                            break;
+
+                        case ('2'):
+                            selectedRoom = "down";
+                            break;
+                    }
+
+                    int xCoord = int.Parse(node[1].ToString());
+                    int yCoord = int.Parse(node[2].ToString());
+
+                    //BAD CODE
+                    if (xCoord == 9 || yCoord == 9)
+                    {
+                        xCoord = 8;
+                        yCoord = 8;
+                    }
+
+                    grid[xCoord, yCoord] = selectedRoom;
+
+                    string roomNum = xCoord.ToString();
+                    roomNum += yCoord.ToString();
+
+                    roomUsed[roomNum] = true;
+
+                    roomCount += 1;
+                }
+            }
+
+            return grid;
+        }   
     }
 }
