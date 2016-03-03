0f418368527c580b1d43547fb047a0fecfdecac8
ianmoon20 <imm3350@g.rit.edu>
2016-03-02 23:30:05 -0500

    Toolkit
    Roombuilder for the game, added to the folder. Should be complete, but
    some minor bugs are known:
    
    -If you go to open a file, you MUST open a file.

---
+++ RoomBuilder/RoomBuilder/MainWindow.xaml.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace RoomBuilder
+{
+   public partial class MainWindow : Window
+    {
+        //Creating an instance of an editor
+        private TileMapEditor editor;
+
+        //The distance the mouse has moved from the origin
+        private double offsetX;
+        private double offsetY;
+
+        //Varible for zooming
+        private double zoom = 1;
+
+        //Previous position of the mouse
+        private Point previousPosition;
+
+        public MainWindow()
+        {
+            //Adding commands to the buttons that can be pressed
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, new ExecutedRoutedEventHandler(NewCommand)));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, new ExecutedRoutedEventHandler(OpenCommand)));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, new ExecutedRoutedEventHandler(SaveCommand)));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, new ExecutedRoutedEventHandler(CloseCommand)));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Properties, new ExecutedRoutedEventHandler(PropertiesCommand)));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Replace, new ExecutedRoutedEventHandler(TilesetCommand)));
+
+            InitializeComponent();
+        }
+
+        //Event Handler for the save button
+        private void SaveCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            //If there is is a TileMapEditor initialized
+            if (editor != null)
+            {
+                Save();
+            }
+        }
+
+        private bool Save()
+        {
+            //Opening the Microsoft box for saving files
+            Microsoft.Win32.SaveFileDialog sdlg = new Microsoft.Win32.SaveFileDialog();
+
+            //Setting the default extension
+            sdlg.DefaultExt = ".txt";
+
+            //Setting the filter
+            sdlg.Filter = "Roombuilder|*.txt";
+
+            //Seeing if the box can be shown
+            bool? result = sdlg.ShowDialog();
+
+            //If the box can be shown
+            if(result == true)
+            {
+                //Save the file
+                editor.Save(sdlg.FileName);
+
+                //Change to nothing has changed
+                editor.hasChanged = false;
+
+                //return that the file saved
+                return true;
+            }
+
+            //If nothing saved, return false
+            return false;
+
+        }
+
+        //Event Handler for loading in a tile set image
+        private void TilesetCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(editor != null)
+            {
+                //Getting the image that will be used as the tileset
+                BitmapImage image = new BitmapImage(new Uri(OpenFile(".png")));
+            }
+        }
+
+        private void PropertiesCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(editor != null)
+            {
+                //Creating a property window box
+                MapPropertyWindow mpw = new MapPropertyWindow();
+
+                //Displaying the box
+                mpw.ShowDialog();
+
+                //Setting the properties to those that are specified in the box
+                editor.SetProperties(int.Parse(mpw.widthBox.Text), int.Parse(mpw.heightBox.Text), int.Parse(mpw.tileWidthBox.Text), int.Parse(mpw.tileHeightBox.Text));
+            }
+        }
+
+        private void CloseCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(editor == null || !editor.hasChanged || Save())
+            {
+                Close();
+            }
+        }
+
+        private void OpenCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            string file = OpenFile(".txt");
+
+            if(file != "")
+            {
+                if (editor != null && editor.hasChanged)
+                {
+                    if(!Save())
+                    {
+                        return;
+                    }
+                }
+
+                else if(editor == null)
+                {
+                    editor = new TileMapEditor(this, new BitmapImage(new Uri(OpenFile(".png"))));
+                }
+
+                editor.LoadMap(file);
+            }
+        }
+
+        private void NewCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(editor != null && editor.hasChanged)
+            {
+                if(!Save())
+                {
+                    return;
+                }
+            }
+
+            editor = NewMap();
+        }
+
+        private TileMapEditor NewMap()
+        {
+            try
+            {
+                MapPropertyWindow mpw = new MapPropertyWindow();
+                mpw.ShowDialog();
+
+                BitmapImage image = new BitmapImage(new Uri(OpenFile(".png")));
+
+                zoom = 1;
+                offsetX = 0;
+                offsetY = 0;
+
+                return new TileMapEditor(this, int.Parse(mpw.widthBox.Text), int.Parse(mpw.heightBox.Text), int.Parse(mpw.tileWidthBox.Text), int.Parse(mpw.tileHeightBox.Text), image);
+            }
+
+            catch
+            {
+                return null;
+            }
+        }
+
+        //Method used to open a file
+        public string OpenFile(string fileType)
+        {
+            //Opening the Windows Box to select a file
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            //Setting the default file extension
+            dlg.DefaultExt = fileType;
+
+            //Filtering the files by the extension
+            dlg.Filter = fileType + "|*" + fileType;
+
+            //Showing the dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            //If the dialog box opens
+            if(result == true)
+            {
+                //Open the selected file
+                return dlg.FileName;
+            }
+
+            //Return an empty string if nothing else is returned
+            return "";
+        }
+
+        public void SetImages(List<CroppedBitmap> images)
+        {
+            listView1.Items.Clear();
+            foreach(var image in images)
+            {
+                listView1.Items.Add(new Image() { Source = image });
+            }
+            listView1.Width = images[0].Width * 2 + 32;
+        }
+
+        public void SetCanvasProperties(int width, int height, int tileWidth, int tileHeight)
+        {
+            canvas1.Children.Clear();
+            canvas1.Children.Add(new Rectangle() { Width = width, Height = height, Stroke = Brushes.LightGray, Fill = Brushes.LightGray });
+            for(int x = 0; x < height; x += tileHeight)
+            {
+                for(int y = 0; y < width; y += tileWidth)
+                {
+                    canvas1.Children.Add(new Rectangle() { Width = tileWidth, Height = tileHeight, Margin = new Thickness(y, x, 0, 0), Stroke = Brushes.Gray, StrokeThickness = 1 });
+                }
+            }
+        }
+
+        private void PressDownMouse(object sender, MouseEventArgs e)
+        {
+            if (editor != null)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed || e.RightButton == MouseButtonState.Pressed)
+                {
+                    MoveMouse(sender, e);
+                }
+                else if (e.MiddleButton == MouseButtonState.Pressed)
+                {
+                    previousPosition = e.GetPosition(this);
+                }
+            }
+
+        }
+
+        private void MoveMouse(object sender, MouseEventArgs e)
+        {
+            if(editor != null)
+            {
+                if(e.LeftButton == MouseButtonState.Pressed)
+                {
+                    editor.Draw(e.GetPosition(canvas1), listView1.SelectedIndex);
+                }
+
+                else if(e.RightButton == MouseButtonState.Pressed)
+                {
+                    editor.Draw(e.GetPosition(canvas1), -1);
+                }
+
+                else if(e.MiddleButton == MouseButtonState.Pressed)
+                {
+                    var position = e.GetPosition(this);
+                    var diff = position - previousPosition;
+                    offsetX = offsetX + diff.X;
+                    offsetY = offsetY + diff.Y;
+
+                    MoveZoom();
+                }
+
+                previousPosition = e.GetPosition(this);
+            }
+        }
+
+        internal void SetCanvasContent(int[,] map, int width, int height, int tileWidth, int tileHeight)
+        {
+            if(editor != null)
+            {
+                canvas2.Children.Clear();
+                for(int col = 0; col < height; col++)
+                {
+                    for(int row = 0; row < width; row++)
+                    {
+                        int index = map[row, col];
+                        if (index != -1 && index < listView1.Items.Count)
+                        {
+                            Image i = (Image)listView1.Items[index];
+                            Image toDraw = new Image();
+                            toDraw.Source = i.Source;
+                            toDraw.Width = tileWidth;
+                            toDraw.Height = tileHeight;
+                            toDraw.Margin = new Thickness(row * tileWidth, col * tileHeight, 0, 0);
+                            canvas2.Children.Add(toDraw);
+                        }
+                    }
+                }
+            }
+        }
+
+        internal void SetCanvasContentDrawOver(int x, int y, int tileWidth, int tileHeight, int index)
+        {
+            if(editor != null)
+            {
+                if(index != -1)
+                {
+                    Image i = (Image)listView1.Items[index];
+                    Image toDraw = new Image();
+                    toDraw.Source = i.Source;
+                    toDraw.Width = tileWidth;
+                    toDraw.Height = tileHeight;
+                    toDraw.Margin = new Thickness(x * tileWidth, y * tileHeight, 0, 0);
+                    canvas2.Children.Add(toDraw);
+                }
+                else
+                {
+                    canvas2.Children.Add(new Rectangle() { Width = tileWidth, Height = tileHeight, Margin = new Thickness(x * tileWidth, y * tileHeight, 0, 0), Stroke = Brushes.Gray, Fill = Brushes.LightGray, StrokeThickness = 1 });
+                }
+            }
+        }
+
+        private void canvas2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(editor != null)
+            {
+                editor.Repaint();
+            }
+        }
+
+        private void canvas1_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (editor != null)
+            {
+                if(e.Delta > 0)
+                {
+                    zoom = zoom * 1.25;
+                }
+
+                else if(e.Delta < 0)
+                {
+                    zoom = zoom / 1.25;
+                }
+
+                MoveZoom();
+            }
+        }
+
+        private void MoveZoom()
+        {
+            if(editor != null)
+            {
+                TransformGroup tg = new TransformGroup();
+                tg.Children.Add(new ScaleTransform(zoom, zoom));
+                tg.Children.Add(new TranslateTransform(offsetX, offsetY));
+
+                canvas1.RenderTransform = tg;
+                canvas2.RenderTransform = tg;
+            }
+        }
+    }
+}
---
+++ RoomBuilder/RoomBuilder/TileMapEditor.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.IO;
+
+namespace RoomBuilder
+{
+    class TileMapEditor
+    {
+        //Creating a main window
+        private MainWindow editorWindow;
+
+        //Creating a tile map that will be used to place images
+        private int[,] tileMap;
+
+        //Height and Width for the tile map
+        private int height;
+        private int width;
+
+        //Bool to see if the user has changed anything since last save
+        public bool hasChanged { get; set; }
+
+        //Height and width for the tiles
+        private int tileWidth = 64;
+        private int tileHeight = 64;
+
+        //Creating a bitmap source for the tile set
+        private BitmapSource tileSet;
+
+        //Creating a list that will store the images in the tileset
+        private List<CroppedBitmap> images = new List<CroppedBitmap>();
+
+        //Parameterized constructor
+        public TileMapEditor(MainWindow window, int width, int height, int tileWidth, int tileHeight, BitmapSource bitmapSrc)
+        {
+            editorWindow = window;
+            tileSet = bitmapSrc;
+            SetProperties(width, height, tileWidth, tileHeight);
+
+            //Content has changed
+            hasChanged = true;
+
+            Initialize();
+        }
+
+        //Parameterized Constructor
+        public TileMapEditor(MainWindow mainWindow, BitmapImage bitmapImg)
+        {
+            editorWindow = mainWindow;
+            tileSet = bitmapImg;
+        }
+
+        //Method used to set the grid and tile variables to whatever the user passes in
+        internal void SetProperties(int width, int height, int tileWidth, int tileHeight)
+        {
+            //If the tilemap has not been initialized yet
+            if (tileMap == null)
+            {
+                //Initialize the map with the width and height that the user specifies
+                tileMap = new int[width, height];
+
+                //Change the height and width of the map to the values passed in
+                this.width = width;
+                this.height = height;
+
+                //Set the tile height and width to the values passed in
+                this.tileWidth = tileWidth;
+                this.tileHeight = tileHeight;
+
+                //Set the rows and colums of the map based on the width and height
+                for (int col = 0; col < height; col++)
+                {
+                    for (int row = 0; row < width; row++)
+                    {
+                        //Fill the array with -1's to show there is not currently an image being stored and instantiate each value
+                        tileMap[row, col] = -1;
+                    }
+                }
+            }
+
+            //If there is already a tilemap
+            else
+            {
+                //Create a new map
+                int[,] newTileMap = new int[width, height];
+
+                //Set the colums and rows of the new map with the passed in values
+                for (int col = 0; col < height; col++)
+                {
+                    for (int row = 0; row < width; row++)
+                    {
+                        //Fill the array with -1's to show there is not currently an image being stored and instantiate each value
+                        newTileMap[row, col] = -1;
+                    }
+                }
+
+                //Set the colums and rows of the old map to the values of the new map
+                for (int col = 0; col < height; col++)
+                {
+                    for (int row = 0; row < width; row++)
+                    {
+                        //Preserving tiles that can fit from the old map.
+                        newTileMap[row, col] = tileMap[row, col];
+                    }
+                }
+
+                //Setting the variables to the newly passed in values
+                this.tileWidth = tileWidth;
+                this.tileHeight = tileHeight;
+
+                this.width = width;
+                this.height = height;
+
+                //Setting the tile Map to be the newly created map
+                tileMap = newTileMap;
+            }
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            //Clear the current list of images
+            images.Clear();
+
+            //Cropping images to the specified height and width from an image
+            for (int y = 0; y < tileSet.PixelHeight; y += tileHeight)
+            {
+                for (int x = 0; x < tileSet.PixelWidth; x += tileWidth)
+                {
+                    try
+                    {
+                        //Adding each image to the image list until the entire image is cropped
+                        CroppedBitmap cb = new CroppedBitmap(tileSet, new System.Windows.Int32Rect(x, y, tileWidth, tileHeight));
+                        images.Add(cb);
+                    }
+
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            editorWindow.SetImages(images);
+            editorWindow.SetCanvasProperties(width * tileWidth, height * tileHeight, tileWidth, tileHeight);
+            editorWindow.SetCanvasContent(tileMap, width, height, tileWidth, tileHeight);
+        }
+
+        internal void Draw(System.Windows.Point point, int p)
+        {
+            //Getting the point clicked upon
+            int x = (int)point.X / tileWidth;
+            int y = (int)point.Y / tileHeight;
+
+            //If the x and y values are within the height and width of the canvas and the spot being clicked
+            //on doesn't equal the tile we are trying to draw already
+            if(x >= 0 && x < width && y >= 0 && y < height && tileMap[x,y] != p)
+            {
+                //Set the tilemap value at the position at the point of the cursor equal to the value passed in
+                tileMap[x, y] = p;
+
+                hasChanged = true;
+
+                editorWindow.SetCanvasContentDrawOver(x, y, tileWidth, tileHeight, p);
+            }
+        }
+
+        //Method to repaint the canvas
+        internal void Repaint()
+        {
+            //Restoring the canvas
+            editorWindow.SetCanvasContent(tileMap, width, height, tileWidth, tileHeight);
+        }
+
+        //Method to save the values in the grid in a text file
+        internal void Save(string filePath)
+        {
+            //Formatting the string
+            StringBuilder sb = new StringBuilder();
+
+            //Going through the grid and writing each value followed by a comma
+            for(int col = 0; col < height; col++)
+            {
+                for (int row = 0; row < width; row++)
+                {
+                    sb.Append(tileMap[row, col] + ",");
+                }
+
+                sb.Remove(sb.Length - 1, 1).AppendLine("");
+            }
+
+            //Writing the string to a file
+            File.WriteAllText(filePath, sb.ToString());
+        }
+
+        //Method to load the tile map
+        internal void LoadMap(string filePath)
+        {
+            //Creating a string array to store the lines in the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            //Setting the height to be equal to the length of the lines array
+            height = lines.Length;
+
+            //Setting the width to the number of entries per line
+            width = lines[0].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            //Set the tileMap to be of the size read from the file
+            tileMap = new int[width, height];
+
+            //Creating two variables to store the values in the lines array
+            int x = 0;
+            int y = 0;
+
+            //For each line in lines
+            foreach(var line in lines)
+            {
+                //Start at the beginning when the line switches
+                x = 0;
+
+                //Then, split up the line and set the value in the tile map to the value
+                foreach(var num in line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    tileMap[x, y] = int.Parse(num);
+
+                    //Move forward one position
+                    x = x + 1;
+                }
+
+                //Move down a row
+                y = y + 1;
+
+                //Initialize and repaint the canvas
+                Initialize();
+                Repaint();
+            }
+        }
+
+        //Setting the tileset
+        private void SetTileset(BitmapImage image)
+        {
+            tileSet = image;
+            Initialize();
+        }
+    }
+}
