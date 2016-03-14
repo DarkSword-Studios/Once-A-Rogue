030f1662d7bfb8e7c0952b8412acbc30321dd0c8
ianmoon20 <imm3350@g.rit.edu>
2016-03-14 18:06:52 -0400

    Cursor and Skills

--- Once A Rogue/Once A Rogue/Cursor.cs
+++ Once A Rogue/Once A Rogue/Cursor.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Once_A_Rogue
 {
     class Cursor
+    //Ian Moon
+    //3/14/2016
+    //This class will keep track of the mouse attributes
     {
+        private MouseState msState;
+        private MouseState prevMsState;
+        
+        public void Update(Room room)
+        {
+            msState = Mouse.GetState();
+
+            if (msState.LeftButton == ButtonState.Pressed && prevMsState.LeftButton == ButtonState.Released)
+            {
+
+            }
+
+        }
     }
 }
--- Once A Rogue/Once A Rogue/Skills.cs
+++ Once A Rogue/Once A Rogue/Skills.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Once_A_Rogue
 {
     class Skills
     {
+        //Attributes
+        private int range;
+
+        public int Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
     }
 }
