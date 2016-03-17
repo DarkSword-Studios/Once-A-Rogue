4dcc243683e6b4125ac0a740b0cb26fae2b26746
ianmoon20 <imm3350@g.rit.edu>
2016-03-17 19:38:34 -0400

    Commenting in the Game done.

--- Once A Rogue/Once A Rogue/Cursor.cs
+++ Once A Rogue/Once A Rogue/Cursor.cs
@@ -13,16 +13,22 @@ namespace Once_A_Rogue
     //3/14/2016
     //This class will keep track of the mouse attributes
     {
+        //Mouse States
         private MouseState msState;
         private MouseState prevMsState;
         
         public void Update(Room room, Player player)
         {
+            //Avix (A.K.A: Stasha's room tagging)
             room.TagTiles(msState.Position, player.CurrSkill.BurstRadius, player.CurrSkill.BurstRadius, player.CurrSkill.BurstRadius, player.CurrSkill.BurstRadius, player.PosRect, player.CurrSkill.RangeX, player.CurrSkill.RangeY);
+
+            //Getting the current state
             msState = Mouse.GetState();
 
+            //Checking for a single press
             if (msState.LeftButton == ButtonState.Pressed && prevMsState.LeftButton == ButtonState.Released)
             {
+                //Activating the player's current skill
                 player.CurrSkill.OnActivated(player);
             }
         }
