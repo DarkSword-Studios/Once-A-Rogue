9efd898b6a54bad1aa60c41ee34d9176ad0d78b3
ianmoon20 <imm3350@g.rit.edu>
2016-04-11 23:24:06 -0400

    Skill selection, Weapon changing, Projectile Drawing, Projectile Updating

--- Once A Rogue/Once A Rogue/Fireball.cs
+++ Once A Rogue/Once A Rogue/Fireball.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace Once_A_Rogue
 {
@@ -11,23 +14,47 @@ namespace Once_A_Rogue
     //3/20/2016
     //This class represents a fireball skill that the Player will be a able to use
     {
-
         //Constructor that takes player object. Has no burst and a cooldown of 5, although it starts castable
         public Fireball()
         {
             Cooldown = 0;
-            CooldownTotal = 5;
+            CooldownTotal = 3000;
             BurstRadius = 0;
             IsDirectional = true;
+            RangeX = 5;
+            RangeY = 5;
         }
 
         //Overide OnActivated method
         public override Boolean OnActivated(Player player)
         {
-            if (base.OnActivated(player))
+            if (Cooldown == 0)
             {
-                Skills.projectiles.Add(new Projectile(1, 0, 0, 7));
-                return true;
+                MouseState ms = Mouse.GetState();
+
+                if (base.OnActivated(player))
+                {
+                    Vector2 target = new Vector2(ms.X, ms.Y) - new Vector2(player.PosX - 10, player.PosY + player.PosRect.Height / 2);
+                    if (target != Vector2.Zero)
+                    {
+                        target.Normalize();
+                    }
+
+                    Game1.CurrProjectiles.Add(new Projectile(target, 0, 7, 40, 40, player.PosX - 10, player.PosY + player.PosRect.Height / 2));
+                    return true;
+                }
+
+                else
+                {
+                    Vector2 target = new Vector2(ms.X, ms.Y) - new Vector2(player.PosX + player.PosRect.Width + 10, player.PosY + player.PosRect.Height / 2);
+                    if (target != Vector2.Zero)
+                    {
+                        target.Normalize();
+                    }
+
+                    Game1.CurrProjectiles.Add(new Projectile(target, 0, 7, 40, 40, player.PosX + player.PosRect.Width + 10, player.PosY + player.PosRect.Height / 2));
+                    return false;
+                }
             }
 
             return false;
