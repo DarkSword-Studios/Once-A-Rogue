e2a773845c33b35b27ac3c340d4955e6964b3edc
Stasha Blank (RIT Student) <sob3966@ad.rit.edu>
2016-04-15 17:00:23 -0400

    Started interactables

---
+++ Once A Rogue/Once A Rogue/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Implemented by: Stasha Blank
+//Team: DarkSword Studios
+//Purpose: Declares how the environment handles an interactable
+//Date Modified: 4/15/16
+
+namespace Once_A_Rogue
+{
+    class Interactable
+    {
+        private Boolean interactable;
+        private Boolean passable;
+        private string type;
+
+        private int xCoord;
+        private int yCoord;
+
+        public Interactable(int tileX, int tileY, string type, Boolean passable, Boolean interactable)
+        {
+            xCoord = tileX;
+            yCoord = tileY;
+            this.type = type;
+            this.passable = passable;
+            this.interactable = interactable;
+        }
+
+        public void Interact()
+        {
+            if (!interactable)
+            {
+                return;
+            }
+        }
+    }
+}
--- Once A Rogue/Once A Rogue/Tile.cs
+++ Once A Rogue/Once A Rogue/Tile.cs
@@ -18,9 +18,24 @@ namespace Once_A_Rogue
         private Boolean invalidTag = false;
         private Boolean validTag = false;
 
+        private Boolean occupied = false;
+
         //Each tile gets its own texture (relative location on the tilemap) and relative location in the room to which it belongs
         private Rectangle relativeImageLocal;
         private Rectangle relativeLocation;
+
+        public Boolean Occupied
+        {
+            get
+            {
+                return occupied;
+            }
+
+            set
+            {
+                occupied = value;
+            }
+        }
 
         //Each private tile attribute has a property (just a general get and set)
         public Boolean InvalidTag
